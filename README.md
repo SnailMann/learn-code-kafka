33839a9b757d840861938f0708f5953909c380b4
Li WenJie <liwenjie@haylion.cn>
2019-03-25 11:25:55 +0800

    First

---
+++ README.md
@@ -0,0 +1,3 @@
+# demo-springboot-kafka
+
+kafka demo
---
+++ src/main/java/com/snailmann/kafka/DemoSpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.snailmann.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootKafkaApplication.class, args);
+    }
+
+}
